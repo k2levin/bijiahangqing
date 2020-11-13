065b366cd8a987bc521eb639861f9a9de45d1e2f
Kevin Khew <k2leving@gmail.com>
2020-11-13 16:52:13 +0800

    add market price page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,121 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MarketPrice v-bind="{ formatPrice, formatPercent, isNegPercent, coins1, coins2 }" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MarketPrice from "./components/MarketPrice.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
-</script>
+    MarketPrice,
+  },
+  data() {
+    return {
+      coins: {
+        bitcoin: {
+          name: "BTC",
+          usd: 0.0,
+          cny: 0.0,
+          usd_24h_change: 0.0,
+        },
+        ethereum: {
+          name: "ETH",
+          usd: 0.0,
+          cny: 0.0,
+          usd_24h_change: 0.0,
+        },
+        "molecular-future": {
+          name: "MOF",
+          usd: 0.0,
+          cny: 0.0,
+          usd_24h_change: 0.0,
+        },
+        hyperdao: {
+          name: "HDAO",
+          usd: 0.0,
+          cny: 0.0,
+          usd_24h_change: 0.0,
+        },
+        hshare: {
+          name: "HC",
+          usd: 0.0,
+          cny: 0.0,
+          usd_24h_change: 0.0,
+        },
+      },
+    };
+  },
+  computed: {
+    coins1: function () {
+      let values = [];
+      values.push(this.coins.bitcoin);
+      values.push(this.coins.ethereum);
+      return values;
+    },
+    coins2: function () {
+      let values = [];
+      values.push(this.coins["molecular-future"]);
+      values.push(this.coins.hyperdao);
+      values.push(this.coins.hshare);
+      return values;
+    },
+  },
+  methods: {
+    log(message) {
+      console.table(message);
+    },
+    formatPrice(num) {
+      if (
+        num.toString().split(".").length > 1 &&
+        num.toString().split(".")[1].length > 1
+      ) {
+        num = num.toString().replace(/\d(?=(\d{3})+\.)/g, "$&,");
+      } else {
+        num = num.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+      }
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+      return num;
+    },
+    formatPercent(num) {
+      num = num.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+      num = num + "%";
+      if (num.toString().includes("-") === false) {
+        num = "+" + num;
+      }
+
+      return num;
+    },
+    isNegPercent(num) {
+      return num.toString().includes("-");
+    }
+  },
+  async created() {
+    let coinNames = [
+      "bitcoin",
+      "ethereum",
+      "molecular-future",
+      "hyperdao",
+      "hshare",
+    ];
+    let url =
+      "https://api.coingecko.com/api/v3/simple/price?ids="+coinNames.join()+"&vs_currencies=usd,cny&include_24hr_change=true";
+    let response = await fetch(url).catch((error) => {
+      console.log(error);
+    });
+    if (response === undefined) {
+      return;
+    }
+    let data = await response.json();
+    coinNames.forEach((coinName) => {
+      if (Object.prototype.hasOwnProperty.call(data, coinName) === true) {
+        this.coins[coinName].usd = data[coinName].usd;
+        this.coins[coinName].cny = data[coinName].cny;
+        this.coins[coinName].usd_24h_change = data[coinName].usd_24h_change;
+      }
+    });
+  },
+};
+</script>
---
+++ src/components/MarketPrice.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <h2>币价行情</h2>
+    <div class="container">
+      <div v-for="coin in coins1" :key="coin.id">
+        <table>
+          <tr>
+            <td v-html="coin.name"></td>
+          </tr>
+          <tr>
+            <td v-html="'$ ' + formatPrice(coin.usd)"></td>
+          </tr>
+          <tr>
+            <td v-html="'¥ ' + formatPrice(coin.cny)"></td>
+          </tr>
+          <tr>
+            <td class="td-gray">24 涨幅</td>
+          </tr>
+          <tr>
+            <td
+              v-html="formatPercent(coin.usd_24h_change)"
+              :class="isNegPercent(coin.usd_24h_change) ? 'td-red' : 'td-green'"
+            ></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+    <br /><br />
+    <div class="container">
+      <div v-for="coin in coins2" :key="coin.id">
+        <table>
+          <tr>
+            <td v-html="coin.name"></td>
+          </tr>
+          <tr>
+            <td v-html="'$ ' + formatPrice(coin.usd)"></td>
+          </tr>
+          <tr>
+            <td v-html="'¥ ' + formatPrice(coin.cny)"></td>
+          </tr>
+          <tr>
+            <td class="td-gray">24 涨幅</td>
+          </tr>
+          <tr>
+            <td
+              v-html="formatPercent(coin.usd_24h_change)"
+              :class="isNegPercent(coin.usd_24h_change) ? 'td-red' : 'td-green'"
+            ></td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MarketPrice",
+  props: {
+    formatPrice: Function,
+    formatPercent: Function,
+    isNegPercent: Function,
+    coins1: Array,
+    coins2: Array,
+  },
+};
+</script>
+
+<style>
+table,
+th,
+td {
+  border: 1px solid #000000;
+  border-collapse: collapse;
+  text-align: center;
+}
+td {
+  width: 200px;
+}
+.container {
+  display: flex;
+}
+.td-gray {
+  color: #BEBEBE;
+}
+.td-green {
+  color: #00FF00;
+}
+.td-red {
+  color: #FF0000;
+}
+</style>
